f71252c21bfd11336a8d25fcf2d0b7b3cfdae724
Artur Waros <arturiptc1@outlook.com>
2020-04-22 20:07:34 +0200

    Poczatek wprowadania danych

--- ConsoleApplication8/ConsoleApplication8.cpp
+++ ConsoleApplication8/ConsoleApplication8.cpp
@@ -1,39 +1,25 @@
-﻿#include<iostream> // Dla WE/WY korzystamy z usług C++
-#include<locale>  // Ustawienia dla j. polskiego
+﻿// Zadanie AP.7.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
 
-using namespace std; // Skrócenie zapisu o std::// Definicja stosu w konwencjach języka C
+#include <iostream>
 
-#define M 64// Maksymalna liczba cyfr binarnych
-
-typedef struct { int S[M], t; } Stos;// t: szczyt stosu
-
-void init (Stos* s) { s->t=0; }       // Inicjuj stos pusty
-
-bool empty(Stos* s) { return s->t==0; }
-bool full(Stos* s) { return s->t==M; }
-int size(Stos* s) { return s->t; } // Liczba elementów
-int space(Stos* s) { return M-s->t; }// Pozostałe miejsce 
-void push(Stos* s, int e)
-	{if(!full(s)) s->S[s->t++] = e; else abort();}
-int pop(Stos* s) {if(!empty(s)) return s->S[--s->t]; abort();}// cd1 ...
-
-int main() {
-	setlocale(LC_ALL, ""); // "Ogonki"
-	Stos Bin, *sp= &Bin; // Stos i wskazanie na Stos
-	init(sp);  // Pominięcie tego ==> KATASTROFA
-	cout <<"Konwersja binarna liczb całkowitych n>0\n"; // Misja 
-	while(true) { // Konwersacja
-	unsigned n;
-	cout<<"\nPodaj liczbę: n = "; cin>>n;
-	if(n==0) break; // Koniec programu
-	cout<<n<<" = "; // Początek wyniku: powtórzenie liczby n
-	while(n>0) {    // Konwersja
-		push(sp, n&1); n >>=1;
-	}
-	cout<<"["<<size(sp)<<"b] "; // Liczba bitów
-	while(!empty(sp)) cout<<pop(sp);  // Wydruk postaci binarnej
-	cout<<"(2)\n";} // Konwersacja
-	cout<<"\nKoniec programu\n";
-	return 0;}
+using namespace std;
 
 
+int main()
+{
+    cout << "Program do wartosciowania dla dodawania, odejmowania i różniczkowania wielomianów zmiennej rzeczywistej o wspolczynnikach calkowitych" << endl;
+
+    int wspolczynnik = 0, wykladnik = 0;
+
+    for (int i = 0; i <= wykladnik + 1; i++)
+    {
+        cout << "Podaj wspolczynnik dla X: " << endl;
+        cin >> wspolczynnik;
+        cout << "Podaj wykladnik dla X :" << endl;
+        cin >> wykladnik;
+    }
+
+
+
+}
